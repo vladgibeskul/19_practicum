4d85386c21c51b6b603e18ebbb130eb03b78b3b0
vladgibeskul <wgv96300@gmail.com>
2025-02-10 21:16:59 +0200

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+        static void Func()
+        {
+            int n = 150; 
+            double a1 = -200;
+            double d = 0.2; 
+                        
+            double sum = (n / 2.0) * (2 * a1 + (n - 1) * d);
+
+            Console.WriteLine("Сумма " + n + " первых членов арифметической прогрессии: " + sum);
+        }
+    }
+}
