cc53b6e91535a484ab82c87ecc62532fff643702
vladgibeskul <wgv96300@gmail.com>
2025-02-10 21:34:24 +0200

    Add files via upload

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+            static void Func()
+        {
+             double currentDistance = 10; 
+        double totalDistance = 10; 
+        int day = 1; 
+
+        while (totalDistance <= 100)
+        {
+            day++;
+            currentDistance *= 1.1; 
+            totalDistance += currentDistance; 
+        }
+
+        Console.WriteLine($"Суммарный пробег превысит 100 км на {day}-й день.");
+           
+        }
+       
+        
+    }
+}
