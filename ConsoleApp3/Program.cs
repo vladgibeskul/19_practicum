315793cf30d49b581a1df37860d340b5887ba82e
vladgibeskul <wgv96300@gmail.com>
2025-02-10 21:08:10 +0200

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+        static void Func()
+        {
+            int vklad1 = 100;
+            int roznica = 50;
+            int sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                int vklad2 = vklad1 + roznica * i;
+
+                sum += vklad2;
+
+            }
+            Console.WriteLine($"Через 10 месяцев {sum} руб.");
+        }
+    }
+}
