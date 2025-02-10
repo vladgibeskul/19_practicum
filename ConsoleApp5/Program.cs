a54389f581c0e3aff0d73f20a20c2de0ccd78156
vladgibeskul <wgv96300@gmail.com>
2025-02-10 21:23:15 +0200

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+        static void Func()
+        {
+            int day = 1;
+            double S = 10;
+            double Roznica = 0.1;
+            double sumS = 10;
+            while (sumS <= 20)
+            {
+                day++;
+                sumS += sumS * Roznica;
+            }
+            Console.Write($"на {day}-й день лыжник пробежит больше 20км");
+        }
+    }
+}
