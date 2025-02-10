4dcfc5bd2034e9cce055914ebe53c394fcb496bf
vladgibeskul <wgv96300@gmail.com>
2025-02-10 20:46:28 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+        static void Func()
+        { 
+            int a1 = 1; 
+            int d = 4; 
+
+            Console.WriteLine("Первые пять членов арифметической прогрессии:");
+
+            for (int i = 0; i < 5; i++)
+            {
+                int Term = a1 + i * d; 
+                Console.Write(Term + " "); 
+            }
+
+            Console.WriteLine(); 
+        }
+
+    }
+}
